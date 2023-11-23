1826d4a98fe5ea2fb0a13fd9a5072492c75b3b2a
shivansh13579 <shivansh0975@gmail.com>
2023-11-23 19:52:49 +0530

    utils code created

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
 
+    app.on("error", (error)=>{
+        console.log("ERRR",error);
+        throw error
+    })
 
-
-
-// (async ()=>{
-//     try {
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     } catch (error) {
-//         console.log("ERROR: ",error);
-//         throw error;
-//     }
-// })()
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+     console.log("MONGO db connection failed !!!", err);
+})
